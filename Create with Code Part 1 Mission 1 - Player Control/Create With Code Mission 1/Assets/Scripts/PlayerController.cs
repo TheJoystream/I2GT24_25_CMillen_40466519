4d2ee7d285fa41e468afe3283bb6232399cc9915
TheJoystream <Dean.millen88@gmail.com>
2024-10-10 20:57:40 +0100

    Test2

--- Create with Code Part 1 Mission 1 - Player Control/Create With Code Mission 1/Assets/Scripts/PlayerController.cs
+++ Create with Code Part 1 Mission 1 - Player Control/Create With Code Mission 1/Assets/Scripts/PlayerController.cs
@@ -13,7 +13,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //We'll make the vehicle move forward
-        transform.Translate(0, 0, 1);
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
     }
 }
